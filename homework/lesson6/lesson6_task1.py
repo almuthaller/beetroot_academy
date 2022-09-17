5d8339875ab9c9b1db883987c409cc6f2afd46f8
Almut Haller <almut@haller-berlin.de>
2022-09-17 17:57:41 +0200

    Lesson 6 Homework

---
+++ homework/lesson6/lesson6_task1.py
@@ -0,0 +1,11 @@
+# Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys 
+# and the number of occurrences as values. 
+
+sentence = input("Type a sentence without punctuation :) ")     # How would we solve it if there was punctuation in the input?
+keys = sentence.casefold().split()
+values = []
+for element in keys:
+    element_count = keys.count(element)
+    values.append(element_count)
+our_dictionary = dict(zip(keys, values))
+print(our_dictionary)
---
+++ homework/lesson6/lesson6_task2.py
@@ -0,0 +1,19 @@
+# Create a function which takes as input two dicts with structure mentioned above, 
+# then computes and returns the total price of stock.
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+total_stock_price = 0
+for key in stock and prices:
+    total_stock_price += stock[key] * prices[key]
+print(total_stock_price)
