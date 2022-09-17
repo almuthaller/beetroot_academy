4c4452bd1dd9d55d81f8bcef1c97bab56768dafc
Almut Haller <almut@haller-berlin.de>
2022-09-17 18:34:28 +0200

    Homework Lesson 7

---
+++ homework/lesson7/lesson7_task1.py
@@ -0,0 +1,11 @@
+# A simple function.
+
+# Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie. 
+# The function should then print "My favorite movie is named {name}".
+
+
+def favorite_movie():
+    my_movie = input("What is your favorite movie? ")
+    print(f"My favorite movie is named {my_movie}.")
+
+favorite_movie()
