d9f580f97d47d1a0049e64b99090ecdd12ae5bea
Almut Haller <almut@haller-berlin.de>
2022-09-24 19:16:33 +0200

    Additional homework

---
+++ additional_homework/sieve_of_eratosthenes.py
@@ -0,0 +1,19 @@
+"""
+Write a Python program using Sieve of Eratosthenes method for computing primes up to a specified number.
+Note: In mathematics, the sieve of Eratosthenes, (Ancient Greek: κόσκινον Ἐρατοσθένους, kóskinon Eratosthénous) 
+one of a number of prime number sieves, is a simple, ancient algorithm for finding all prime numbers up to any given limit.
+"""
+
+def sieve_of_eratosthenes(limit):
+    prime_numbers = []
+    multiples = []
+
+    for i in range(2, limit + 1):
+        if i not in multiples:
+            prime_numbers.append(i)
+            multiples.extend([x * i for x in range(int(limit / i))])
+        
+    return prime_numbers
+
+
+print(sieve_of_eratosthenes(100))
