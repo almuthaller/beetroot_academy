e8f660774a4549ab081653afc427469eb2ab6f60
Almut Haller <almut@haller-berlin.de>
2022-10-09 01:19:06 +0200

    Added time zone converter

--- other_fun_stuff/unit_converter.py
+++ other_fun_stuff/unit_converter.py
@@ -16,18 +16,30 @@ def convert(value, unit):
     elif unit == "”":
         output_value, output_unit = value * 2.54, "cm"
 
+    # for time zones
+    elif unit == "here":
+        output_value, output_unit = (
+            value - 6 if value >= 6 else (value + 24) - 6,
+            "Bailey's time",
+        )
+    elif unit == "nyc":
+        output_value, output_unit = (
+            value + 6 if value < 18 else (value - 24) + 6,
+            "my time",
+        )
+
     # for undefined unit
     else:
         raise ValueError("There is currently no function to convert from this unit.")
 
-    return f"= {round(output_value, 2)}{output_unit}"
+    return f"= {round(output_value, 2)} {output_unit}"
 
 
-assert convert(13, "°C") == "= 55.4°F"
-assert convert(23.00, "°F") == "= -5.0°C"
+assert convert(13, "°C") == "= 55.4 °F"
+assert convert(23.00, "°F") == "= -5.0 °C"
 
-assert convert(40.00000, "cm") == "= 15.75”"
-assert convert(5, "”") == "= 12.7cm"
+assert convert(40.00000, "cm") == "= 15.75 ”"
+assert convert(5, "”") == "= 12.7 cm"
 
 
 while True:
