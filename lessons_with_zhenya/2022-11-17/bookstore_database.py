0aa5a87a6c7f20e63e83c9c1d56d2ecfbb5ebc9f
Almut Haller <almut@haller-berlin.de>
2022-11-20 12:12:36 +0100

    Today's update

--- lessons_with_zhenya/2022-11-17/bookstore_database.py
+++ lessons_with_zhenya/2022-11-17/bookstore_database.py
@@ -13,18 +13,22 @@ import sqlite3
 
 
 class Item:
-    def __init__(self, name, item_type, price, quantity):
+    registry = []
+
+    def __init__(self, item_id, name, item_type, price, quantity):
+        self.id = item_id
         self.name = name
         self.item_type = item_type
         self.price = price
         self.quantity = quantity
-        # We might also want to have an attribute with an id number to more easily identify items.
+        self.registry.append(self)
 
     @classmethod
     def create_table(cls, connection):
         with connection:
             connection.cursor().execute(
                 """CREATE TABLE IF NOT EXISTS items (
+                id integer PRIMARY KEY AUTOINCREMENT,
                 name text,
                 type text,
                 price integer,
@@ -32,38 +36,58 @@ class Item:
                 )"""
             )
 
-    def add_to_table(self, connection):
+    @classmethod
+    def create_or_update(cls, connection, name, item_type, price, quantity):
         with connection:
             cursor = connection.cursor()
             cursor.execute(
-                "SELECT name, quantity FROM items WHERE name = :name and type = :type",
-                {"name": self.name, "type": self.item_type},
+                "SELECT id, quantity FROM items WHERE name = :name and type = :type",
+                {"name": name, "type": item_type},
             )
             matches = cursor.fetchall()
-            if len(matches) > 0:
-                assert len(matches) == 1
-                match_name, match_quantity = matches[0]
-                assert match_name == self.name
 
+            if len(matches) == 0:
+                # create new instance
+                cursor.execute(
+                    f"""INSERT INTO items(name, type, price, quantity) VALUES (:name, :item_type, :price, :quantity)""",
+                    {
+                        "name": name,
+                        "item_type": item_type,
+                        "price": price,
+                        "quantity": quantity,
+                    },
+                )
+
+                cursor.execute(
+                    "SELECT id FROM items WHERE name = :name and type = :type",
+                    {"name": name, "type": item_type},
+                )
+                object_id = cursor.fetchone()
+
+                return cls(object_id, name, item_type, price, quantity)
+
+            elif len(matches) == 1:
+                # update quantity
                 cursor.execute(
                     """UPDATE items 
                     SET quantity = :quantity 
-                    WHERE name = :name and type = :type""",
+                    WHERE id = :id""",
                     {
-                        "quantity": int(match_quantity) + self.quantity,
-                        "name": self.name,
-                        "type": self.item_type,
+                        "quantity": int(matches[0][1]) + quantity,
+                        "id": matches[0][0],
                     },
                 )
+
+                for item in cls.registry:
+                    if item.id == matches[0][0]:
+                        item.quantity += quantity
+                        return item
+
+                return cls(object_id, name, item_type, price, quantity)
+
             else:
-                cursor.execute(
-                    f"""INSERT INTO items VALUES (:name, :item_type, :price, :quantity)""",
-                    {
-                        "name": self.name,
-                        "item_type": self.item_type,
-                        "price": self.price,
-                        "quantity": self.quantity,
-                    },
+                raise sqlite3.DataError(
+                    f"You have a duplicate of name {name}, type {item_type} in your database."
                 )
 
     def delete_all(self, connection):
@@ -71,23 +95,35 @@ class Item:
 
 
 if __name__ == "__main__":
-    postcard1 = Item("Happy Birthday card", "postcard", 1, 1)
+    """postcard1 = Item("Happy Birthday card", "postcard", 1, 1)
     postcard2 = Item("Happy Birthday card", "postcard", 1, 4)
     # One problem is that this code allows you to have two instances for the "same" item with DIFFERENT prices.
     # That doesn't make sense and might be a reason to restructure the database. For now, I left it like this.
 
     notebook1 = Item("green notebook", "notebook", 6, 3)
 
-    connection = sqlite3.connect("lessons_with_zhenya/2022-11-17/bookstore.db")
-    cursor = connection.cursor()
-
-    Item.create_table(connection)
-
     postcard1.delete_all(connection)
 
     postcard1.add_to_table(connection)
     postcard2.add_to_table(connection)
     notebook1.add_to_table(connection)
+    """
 
-    cursor.execute("SELECT * FROM items")
+    connection = sqlite3.connect("lessons_with_zhenya/2022-11-17/bookstore.db")
+    cursor = connection.cursor()
+
+    Item.create_table(connection)
+
+    cursor.execute("SELECT id FROM items")
     print(cursor.fetchall())
+
+    # cursor.execute("DROP TABLE items")
+
+    new_test_object = Item.create_or_update(
+        connection, "Test name 1", "Test type", 10, 2
+    )
+    print(new_test_object)
+    # cursor.execute(
+    #    "SELECT * FROM items WHERE name = 'Test name' and type = 'Test type'"
+    # )
+    # print(cursor.fetchall())
