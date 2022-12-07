54698a59ed73084e45c39720ca8891a1077fdae9
Almut Haller <almut@haller-berlin.de>
2022-12-07 21:35:06 +0100

    Updated homework

--- lessons_with_zhenya/2022-12-06/odered_linked_list.py
+++ lessons_with_zhenya/2022-12-06/odered_linked_list.py
@@ -25,6 +25,9 @@ class OrderedLinkedList:
         else:
             raise StopIteration
 
+    def __str__(self):
+        return str([x.value for x in self])
+
     def add_node(self, value):
         node = Node(value)
         if self.head is None:
@@ -51,7 +54,7 @@ class OrderedLinkedList:
             else:
                 previous.next = node
 
-    def find_middle_impl(self, first, last):
+    def __find_middle(self, first, last):
         if first is None:
             return None
         slow = first
@@ -69,7 +72,7 @@ class OrderedLinkedList:
         first = self.head
         last = None
         while True:
-            middle = self.find_middle_impl(first, last)
+            middle = self.__find_middle(first, last)
             if middle is None:
                 return False
             if middle.value == value:
@@ -84,10 +87,34 @@ class OrderedLinkedList:
 
 
 our_list = OrderedLinkedList()
+
+assert not our_list.binary_search(1)
+
 our_list.add_node(1)
 our_list.add_node(4)
 our_list.add_node(2)
 our_list.add_node(2)
-for i in our_list:
-    print(i)
-print(our_list.binary_search(3))
+
+assert str(our_list) == "[1, 2, 4]"
+
+assert not our_list.binary_search(3)
+assert not our_list.binary_search(0)
+assert not our_list.binary_search(5)
+
+assert our_list.binary_search(1)
+assert our_list.binary_search(2)
+assert our_list.binary_search(4)
+
+our_list.add_node(6)
+
+assert str(our_list) == "[1, 2, 4, 6]"
+
+assert not our_list.binary_search(3)
+assert not our_list.binary_search(0)
+assert not our_list.binary_search(5)
+assert not our_list.binary_search(7)
+
+assert our_list.binary_search(1)
+assert our_list.binary_search(2)
+assert our_list.binary_search(4)
+assert our_list.binary_search(6)
