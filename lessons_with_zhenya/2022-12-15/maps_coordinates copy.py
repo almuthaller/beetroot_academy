fd472906809ea1bd6e2cd8fe35519efb799c2d6a
Almut Haller <almut@haller-berlin.de>
2022-12-18 10:41:36 +0100

    Homework

---
+++ lessons_with_zhenya/2022-12-15/maps_coordinates copy.py
@@ -0,0 +1,24 @@
+"""
+Create simple command-line application that asks user to input an address and returns coordinates of that point 
+(latitude and longitude). To obtain that information you may use Google Maps API for Geocoding.
+"""
+
+from urllib.parse import quote
+import requests
+
+
+def get_coordinates(location):
+    location_formatted = quote(location)
+    key = ""
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    full_url = base_url + f"?address={location_formatted}&key={key}"
+
+    response = requests.get(full_url)
+    assert response.status_code == 200
+
+    return response.json()["results"][0]["geometry"]["location"]
+
+
+print(get_coordinates("Sveavägen 42"))
+print(get_coordinates("Brandenburger Tor")["lat"])
